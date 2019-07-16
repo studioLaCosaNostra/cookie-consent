9be27db22f9ae2fe9ce441ba4d52f2e1f5d2993e
Travis CI <travis@travis-ci.org>
2019-07-16 13:41:52 +0000

    Travis update (21)
    [skip ci]

--- dist/ngx-cookie-consent/esm5/ngx-cookie-consent.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { NgxCookieConsentComponent, NgxCookieConsentModule } from './public-api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmd4LWNvb2tpZS1jb25zZW50LmpzIiwic291cmNlUm9vdCI6Im5nOi8vbmd4LWNvb2tpZS1jb25zZW50LyIsInNvdXJjZXMiOlsibmd4LWNvb2tpZS1jb25zZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFJQSxrRUFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljLWFwaSc7XG4iXX0=
