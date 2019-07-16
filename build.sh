89bd0e86f085f4d0a5ee0139985be7dc927784e3
Paweł Łaski <zniwus@gmail.com>
2019-07-16 16:08:30 +0200

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-APP=cookie-consent 
+APP=cookie-consent
 ng build $APP --prod --output-hashing=none && cat dist/$APP/runtime-es5.js dist/$APP/polyfills-es5.js dist/$APP/scripts.js dist/$APP/main-es5.js > dist/$APP/$APP.js
