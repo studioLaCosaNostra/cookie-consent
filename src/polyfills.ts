9c67aba461f434a6dbd7bd5f4581b8093af9a6d4
Paweł Łaski <zniwus@gmail.com>
2019-11-09 14:43:59 +0100

    remove custom elements adapter

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,4 +61,4 @@
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
+// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
